e24f2ec32b308c58f5ecf5b92bc6071bd1dfcd10
Alex Biswas <62838188+biswasalex410@users.noreply.github.com>
2020-09-13 13:44:11 +0600

    Add files via upload

---
+++ OOP/src/oop/Test.java
@@ -0,0 +1,14 @@
+
+package oop;
+
+public class Test {
+    public static void main(String[] args) {
+  
+        Teacher teacher1 = new Teacher("Alex Biswas", "Male",175419641);
+        teacher1.displayInformation();
+        
+        Teacher teacher2 = new Teacher("Maxi Biswas", "Male", 629215756);
+        teacher2.displayInformation();
+    }
+ 
+}
